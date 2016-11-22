cd66d4c115e7257eb5582844a21e2c2cbd6e5f0e
Ian Price <djianprice@gmail.com>
2016-11-22 15:25:52 -0800

    Update preprocessors.py

--- webodt/preprocessors.py
+++ webodt/preprocessors.py
@@ -65,7 +65,7 @@ def xmlfor_preprocessor(template_content):
         if re_endxmlfor_match:
             try:
                 start_el, forloop_clause = xmlfor_starts.pop()
-            except IndexError, e:
+            except IndexError as e:
                 raise ValueError('Unexpected {%% endxmlfor %%} tag near %s' % el.text)
             xmlfor_pairs.append((start_el, el, forloop_clause))
             el.text = re_endxmlfor.sub('', el.text)
